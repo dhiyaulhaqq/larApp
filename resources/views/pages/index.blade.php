f10e53ff8774bb97116da854a6c8b824d43c7e0b
dhiyaulhaqq <dhiyaulhaqq@gmail.com>
2019-04-18 12:47:58 +0700

    change format to extend app layout

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,12 +1,6 @@
-<!doctype html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Laravel App</title>
-    </head>
-    <body>
-        <h1>Welcome To Laravel</h1>
-        <p>This is the laravel application from the "Laravel from Scratch" Youtube series</p>
-    </body>
-</html>
+@extends('layouts.app')
+
+@section('content')
+    <h1>Welcome To Laravel</h1>
+    <p>This is the laravel application from the "Laravel from Scratch" Youtube series</p>
+@endsection
--- resources/views/pages/services.blade.php
+++ resources/views/pages/services.blade.php
@@ -1,12 +1,6 @@
-<!doctype html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Laravel App</title>
-    </head>
-    <body>
-        <h1>Services</h1>
-        <p>This is the services page</p>
-    </body>
-</html>
+@extends('layouts.app')
+
+@section('content')
+    <h1>Services</h1>
+    <p>This is the services page</p>
+@endsection
