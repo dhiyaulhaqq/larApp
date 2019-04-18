8b49ecf737f8125117ea5e0da9935ebae01bd621
dhiyaulhaqq <dhiyaulhaqq@gmail.com>
2019-04-18 13:47:06 +0700

    pass variable into pages

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -7,7 +7,8 @@ use Illuminate\Http\Request;
 class PagesController extends Controller
 {
     public function index(){
-        return view('pages.index');
+        $name = 'Iqbal';
+        return view('pages.index')->with('name', $name);
     }
 
     public function about(){
--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>Welcome To Laravel</h1>
+    <h1>Welcome {{ $name }}</h1>
     <p>This is the laravel application from the "Laravel from Scratch" Youtube series</p>
 @endsection
