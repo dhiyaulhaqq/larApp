53f7d2c78919c189f2f30eba54ec5015e185ae8e
dhiyaulhaqq <dhiyaulhaqq@gmail.com>
2019-04-19 21:55:12 +0700

    make delete controller

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -9,5 +9,12 @@
     <small>Created at {{ $post->created_at }}</small>
     <h1>{{ $post->title }}</h1>
     <p>{{ $post->body }}</p>
-    <p><a href="/posts/{{ $post->id }}/edit" class="btn btn-dark">Edit</a></p>
+    <div class="d-flex flex-row">
+        <a href="/posts/{{ $post->id }}/edit" class="btn btn-dark mr-2">Edit</a>
+        <form action="/posts/{{ $post->id }}" method="POST">
+            @method('DELETE')
+            @csrf
+            <button type="submit" class="btn btn-danger">Delete</button>
+        </form>
+    </div>
 @endsection
