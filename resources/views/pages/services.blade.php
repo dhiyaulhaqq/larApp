3056471d9760bc743e7738746b116875e3d3ee47
dhiyaulhaqq <dhiyaulhaqq@gmail.com>
2019-04-21 15:08:47 +0700

    change posts & services view

--- resources/views/pages/services.blade.php
+++ resources/views/pages/services.blade.php
@@ -5,10 +5,14 @@
 @endsection
 
 @section('content')
-    <h1>Services</h1>
-    <ul class="list-group">
-    @foreach ($services as $service)
-        <li class="list-group-item">{{ $service }}</li>
-    @endforeach
-    </ul>
+    <div class="row">
+        <div class="col-md-8">
+            <h1>Services</h1>
+            <ul class="list-group">
+            @foreach ($services as $service)
+                <li class="list-group-item">{{ $service }}</li>
+            @endforeach
+            </ul>
+        </div>
+    </div>
 @endsection
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -5,20 +5,24 @@
 @endsection
 
 @section('content')
-    <h1>Posts</h1>
-    @if (count($posts) > 0)
-        <ul class="list-group mb-3">
-            @foreach ($posts as $post)
-                <li class="list-group-item">
-                    <a href="/posts/{{ $post->id }}" class="text-dark">
-                        <h3>{{ $post->title }}</h3>
-                    </a>
-                    <small>Created at {{ $post->created_at }}</small>
-                </li>
-            @endforeach
-        </ul>
-        {{ $posts->links() }}
-    @else
-        <p>No posts</p>
-    @endif
+    <div class="row">
+        <div class="col-md-8">
+            <h1>Posts</h1>
+            @if (count($posts) > 0)
+                <ul class="list-group mb-3">
+                    @foreach ($posts as $post)
+                        <li class="list-group-item">
+                            <a href="/posts/{{ $post->id }}" class="text-dark">
+                                <h4 class="mb-0">{{ $post->title }}</h4>
+                            </a>
+                            <small>Created at {{ $post->created_at }}</small>
+                        </li>
+                    @endforeach
+                </ul>
+                {{ $posts->links() }}
+            @else
+                <p>No posts</p>
+            @endif
+        </div>
+    </div>
 @endsection
