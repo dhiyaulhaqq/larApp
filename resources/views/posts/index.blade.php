dd9b5835e08a996c0aec1962dd5d9431579e5134
dhiyaulhaqq <dhiyaulhaqq@gmail.com>
2019-04-22 09:35:11 +0700

    change index posts page

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -6,20 +6,27 @@
 
 @section('content')
     <div class="row">
-        <div class="col-md-8">
+        <div class="col-md-12">
             <h1>Posts</h1>
             @if (count($posts) > 0)
-                <ul class="list-group mb-3">
-                    @foreach ($posts as $post)
-                        <li class="list-group-item">
-                            <a href="/posts/{{ $post->id }}" class="text-dark">
-                                <h4 class="mb-0">{{ $post->title }}</h4>
-                            </a>
-                            <small>Created at {{ $post->created_at }}</small>
-                        </li>
-                    @endforeach
-                </ul>
-                {{ $posts->links() }}
+                <table class="table table-striped">
+                    <thead>
+                        <tr>
+                            <th>Post Title</th>
+                            <th>Created at</th>
+                            <th>Posted by</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($posts as $post)
+                        <tr>
+                            <td><a href="/posts/{{ $post->id }}" class="text-dark">{{ $post->title }}</a></td>
+                            <td>{{ $post->created_at }}</td>
+                            <td>{{ $post->user->name }}</td>
+                        </tr>
+                        @endforeach
+                    </tbody>
+                </table>
             @else
                 <p>No posts</p>
             @endif
