2476deb9356c59c41b9349d13702f9333bf6a49c
dhiyaulhaqq <dhiyaulhaqq@gmail.com>
2019-04-22 20:14:16 +0700

    extract posts content in index page to posts page

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -5,31 +5,5 @@
 @endsection
 
 @section('content')
-    <div class="row">
-        <div class="col-md-12">
-            <h1>Posts</h1>
-            @if (count($posts) > 0)
-                <table class="table table-striped">
-                    <thead>
-                        <tr>
-                            <th>Post Title</th>
-                            <th>Created at</th>
-                            <th>Posted by</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        @foreach ($posts as $post)
-                        <tr>
-                            <td><a href="/posts/{{ $post->id }}" class="text-dark">{{ $post->title }}</a></td>
-                            <td>{{ $post->created_at }}</td>
-                            <td>{{ $post->user->name }}</td>
-                        </tr>
-                        @endforeach
-                    </tbody>
-                </table>
-            @else
-                <p>No posts</p>
-            @endif
-        </div>
-    </div>
+    @include('includes.posts')
 @endsection
