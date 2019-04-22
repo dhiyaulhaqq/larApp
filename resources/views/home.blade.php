bba412da88edc03eb053a7070f095a76d2ac04c9
dhiyaulhaqq <dhiyaulhaqq@gmail.com>
2019-04-22 09:46:08 +0700

    create user dahsboard to show user posts

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,41 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
-                </div>
-            </div>
-        </div>
+    <div class="alert alert-success">
+        You are logged in!
     </div>
-</div>
+    <h1>Dashboard</h1>
+    <p><a href="{{ route('posts.create') }}" class="btn btn-dark">{{ __('Create Post') }}</a></p>
+    <h3>Your Blog Posts</h3>
+    @if (count($posts) > 0)
+        <table class="table table-striped">
+            <thead>
+                <tr>
+                    <th>Post Title</th>
+                    <th>Created at</th>
+                    <th>Action</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($posts as $post)
+                <tr>
+                    <td><a href="/posts/{{ $post->id }}" class="text-dark">{{ $post->title }}</a></td>
+                    <td>{{ $post->created_at }}</td>
+                    <td>
+                        <div class="d-flex flex-row">
+                            <a href="/posts/{{ $post->id }}/edit" class="btn btn-dark mr-2">Edit</a>
+                            <form action="/posts/{{ $post->id }}" method="POST">
+                                @method('DELETE')
+                                @csrf
+                                <button type="submit" class="btn btn-danger">Delete</button>
+                            </form>
+                        </div>
+                    </td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+    @else
+        <p>No posts</p>
+    @endif
 @endsection
